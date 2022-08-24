b8935f600b62425539f72b3386f5181f0952ef43
Jayson Jarabe <jaesharp19@gmail.com>
2022-08-24 10:13:16 +0800

    order-range

--- controllers/order/get.ts
+++ controllers/order/get.ts
@@ -8,14 +8,14 @@ const findAsync = async (_id?: string, from?: string, to?: string, sort?: string
 			$and: [
 				from
 					? {
-							createdAt: {
+							'delivery.date': {
 								$gte: Moment(from, process.env.DATE_FORMAT).startOf('day').toDate()
 							}
 					  }
 					: {},
 				to
 					? {
-							createdAt: {
+							'delivery.date': {
 								$lte: Moment(to, process.env.DATE_FORMAT).endOf('day').toDate()
 							}
 					  }
