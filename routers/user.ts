54ead2fb84447b2e1e7a7cbc42dd20aeae5b1548
Jayson Jarabe <jaesharp19@gmail.com>
2022-09-13 22:53:40 +0800

    user

--- routers/user.ts
+++ routers/user.ts
@@ -4,7 +4,7 @@ import { addOne, login } from '../handlers/user/post'
 const router = Router()
 
 router.post('/', addOne)
-router.get('/login', login)
+router.post('/login', login)
 // router.get('/', getAll)
 // router.get('/:_id', getById)
 
